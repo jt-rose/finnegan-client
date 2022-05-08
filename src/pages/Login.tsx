5e7b2a1d62f2a06229a8f6fb2a7ebb8703abf097
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-08 17:59:43 -0400

    remove unused code from login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,30 +1,11 @@
-import { useMutation } from "react-query";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { User } from "../models/User";
 
 function Login() {
   const navigate = useNavigate();
   const login = () => {
-    User.login("user", "password");
+    User.login("user", "password").then(() => navigate("/"));
   };
-  // const loginMutation = useMutation(
-  //   (config: { username: string; password: string }) =>
-  //     axios.post(LOGIN_ROUTE, config),
-  //   {
-  //     onSuccess: (data) => {
-  //       console.log(data);
-  //       const token = data.headers.authorization;
-  //       console.log(token);
-  //       sessionStorage.setItem("token", token);
-  //       navigate("/");
-  //     },
-  //   }
-  // );
-
-  // const handleLogin = () => {
-  //   loginMutation.mutate({ username: "user", password: "password" });
-  // };
 
   return <p onClick={login}>login</p>;
 }
