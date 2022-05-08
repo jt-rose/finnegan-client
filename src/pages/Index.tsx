406cf2202df65ddad0de4e5a7d6c0c5dc6129499
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-08 17:45:44 -0400

    update model methods

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,23 +1,20 @@
-import axios from "axios";
-import { useMutation } from "react-query";
-import { post } from "../queries/fetchers";
-import { useGetRecurringTransactionsQuery } from "../queries/recurringTransactionController";
-import { useGetUserQuery } from "../queries/userController";
-import { ITransaction, Transaction } from "../models/Transaction";
+import { Transaction } from "../models/Transaction";
+import { RecurringTransaction } from "../models/RecurringTransaction";
+import { User } from "../models/User";
 
 const Index = () => {
-  const { isLoading, error, data } = useGetUserQuery();
+  const { isLoading, error, data } = User.useFetch();
 
   console.log(isLoading);
   console.log("data: ", data);
   console.log("error: ", error);
 
-  const res = Transaction.fetch(); //useGetTransactionsQuery();
+  const res = Transaction.useFetch(); //useGetTransactionsQuery();
   console.log("transaction: ", res.isLoading);
   console.log("transaction data: ", res.data);
   console.log("transaction error: ", res.error);
 
-  const res2 = useGetRecurringTransactionsQuery();
+  const res2 = RecurringTransaction.useFetch();
   console.log("recurring: ", res2.isLoading);
   console.log("recurring data: ", res2.data);
   console.log("recurring error: ", res2.error);
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,29 +1,32 @@
 import { useMutation } from "react-query";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { LOGIN_ROUTE } from "../queries/userController";
+import { User } from "../models/User";
 
 function Login() {
   const navigate = useNavigate();
-  const loginMutation = useMutation(
-    (config: { username: string; password: string }) =>
-      axios.post(LOGIN_ROUTE, config),
-    {
-      onSuccess: (data) => {
-        console.log(data);
-        const token = data.headers.authorization;
-        console.log(token);
-        sessionStorage.setItem("token", token);
-        navigate("/");
-      },
-    }
-  );
-
-  const handleLogin = () => {
-    loginMutation.mutate({ username: "user", password: "password" });
+  const login = () => {
+    User.login("user", "password");
   };
+  // const loginMutation = useMutation(
+  //   (config: { username: string; password: string }) =>
+  //     axios.post(LOGIN_ROUTE, config),
+  //   {
+  //     onSuccess: (data) => {
+  //       console.log(data);
+  //       const token = data.headers.authorization;
+  //       console.log(token);
+  //       sessionStorage.setItem("token", token);
+  //       navigate("/");
+  //     },
+  //   }
+  // );
 
-  return <p onClick={handleLogin}>login</p>;
+  // const handleLogin = () => {
+  //   loginMutation.mutate({ username: "user", password: "password" });
+  // };
+
+  return <p onClick={login}>login</p>;
 }
 
 export default Login;
