e6732110e30eb1c297f391bea9adb190e0c2d16c
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-07 23:38:11 -0400

    set up Transaction class and persisted interface

--- src/types/Transaction.ts
+++ src/types/Transaction.ts
@@ -1,10 +1,23 @@
 import { User } from "./User";
 
-export interface Transaction {
-  id: number;
+// The Transaction class is used to construct a Transaction entity
+// and the ITransaction interface references a Transaction
+// that has been stored in the database and been assigned
+// a transaction id and associated owner
+export class Transaction {
   amount: number;
   category: string;
   date: Date;
   note?: string;
+
+  constructor(amount: number, category: string, note?: string) {
+    this.amount = amount;
+    this.category = category;
+    this.date = new Date();
+    this.note = note;
+  }
+}
+export interface ITransaction extends Transaction {
+  id: number;
   owner: User;
 }
