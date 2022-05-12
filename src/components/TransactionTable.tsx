93b672b92ca3db4d95bac8743b7e139bd612f49d
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-11 22:54:58 -0400

    add category icons to table

--- src/components/TransactionTable.tsx
+++ src/components/TransactionTable.tsx
@@ -7,6 +7,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { ITransaction } from "../models/Transaction";
+import { CategoryIcon } from "./CategoryIcons";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -28,24 +29,6 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 export default function TransactionTables(props: {
   transactions: ITransaction[];
 }) {
@@ -66,7 +49,8 @@ export default function TransactionTables(props: {
                 {transaction.amount}
               </StyledTableCell>
               <StyledTableCell align="right">
-                {transaction.category}
+                {/*transaction.category*/}
+                <CategoryIcon category={transaction.category} />
               </StyledTableCell>
               <StyledTableCell align="right">{`${transaction.date}`}</StyledTableCell>
             </StyledTableRow>
