d4c7abdf3f4c321081af9ba2d09eccf79d18cf92
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-14 01:41:39 -0400

    set up CRUD buttons

---
+++ src/components/CrudButtons.tsx
@@ -0,0 +1,44 @@
+import { Button, Tooltip } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import EditIcon from "@mui/icons-material/Edit";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+
+const CreateButton = () => {
+  return (
+    <Button variant="contained" endIcon={<AddCircleOutlineIcon />}>
+      Create
+    </Button>
+  );
+};
+
+const ReadMoreButton = () => {
+  return (
+    <Tooltip title="Show more">
+      <MoreHorizIcon />
+    </Tooltip>
+  );
+};
+
+const EditButton = () => {
+  return (
+    <Button variant="contained" color="warning" endIcon={<EditIcon />}>
+      Edit
+    </Button>
+  );
+};
+
+const DeleteButton = () => {
+  return (
+    <Button variant="contained" color="error" endIcon={<HighlightOffIcon />}>
+      Delete
+    </Button>
+  );
+};
+
+export const CRUDButtons = {
+  CreateButton,
+  ReadMoreButton,
+  EditButton,
+  DeleteButton,
+};
